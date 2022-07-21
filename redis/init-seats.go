440fc928815e8a636d548029af4033d8e137f2a3
suhavni <suhavni@gmail.com>
2022-07-21 21:44:51 +0700

    fixed timeslot in initseat

--- redis/init-seats.go
+++ redis/init-seats.go
@@ -11,7 +11,7 @@ func initSeats() {
 	for theater := 1; theater <= 5; theater++ {
 		theater := int64(theater)
 		for timeSlot := 1; timeSlot <= 5*5*5; timeSlot++ {
-			timeSlot := (theater-1)*125 + int64(timeSlot)
+			timeSlot := int64(timeSlot)
 			for seat := 1; seat <= utils.TertiaryOperator(timeSlot == 263, 20, 40).(int); seat++ {
 				seatStatus, err := ReadStatus(theater, timeSlot, int64(seat))
 				if err == nil && seatStatus != AVAILABLE {
